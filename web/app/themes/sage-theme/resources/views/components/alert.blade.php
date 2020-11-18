da703def6656578d0bac5a475cc67b577d36ebe0
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2020-11-17 22:02:43 -0500

    Remove themes

--- web/app/themes/sage-theme/resources/views/components/alert.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="alert alert-{{ $type }} js-alert-dismissible" role="alert">
-    {{ $slot }}
-    <button type="button" class="close" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
--- web/app/themes/sage-theme/resources/views/icons/icon.blade.php
+++
@@ -1,6 +0,0 @@
-@php
-  $iconUrl = "images/icons-sprite.svg#{$name}"
-@endphp
-<svg class="{{ $classes }}" fill="none" stroke="currentColor">
-  <use xlink:href="@asset($iconUrl)" />
-</svg>
